7de225f9840c9495672985ca0d0b8282b5fb8522
YogendraSontakke <yogendra.sontakke@gmail.com>
2015-11-03 19:22:09 +0530

    updated batch files

--- bse/bserun.bat
+++ bse/bserun.bat
@@ -1,2 +1,2 @@
-D:\Python27\python.exe E:\GitHub\BSENSE\bse\get_data_for_companies.py E:/Programming/Personal/bse/testing/json_daily
-D:\Python27\python.exe E:\GitHub\BSENSE\bse\combine_jsons_in_one.py  E:/Programming/Personal/bse/testing/json_daily E:/Programming/Personal/bse/testing/json_consolidated
+D:\Python27\python.exe E:\GitHub\BSENSE\bse\get_data_for_companies.py E:/Programming/Personal/bse/bse_52_week_data/json_daily
+D:\Python27\python.exe E:\GitHub\BSENSE\bse\combine_jsons_in_one.py  E:/Programming/Personal/bse/bse_52_week_data/json_daily E:/Programming/Personal/bse/bse_52_week_data/json_consolidated
---
+++ bse/bserun_testing.bat
@@ -0,0 +1,2 @@
+D:\Python27\python.exe E:\GitHub\BSENSE\bse\get_data_for_companies.py E:/Programming/Personal/bse/testing/json_daily
+D:\Python27\python.exe E:\GitHub\BSENSE\bse\combine_jsons_in_one.py  E:/Programming/Personal/bse/testing/json_daily E:/Programming/Personal/bse/testing/json_consolidated
