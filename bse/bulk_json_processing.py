34e2968c588de13e4314b4b0963a9f4714d09b7f
YogendraSontakke <yogendra.sontakke@gmail.com>
2015-11-03 19:37:57 +0530

    Updated for path handling

--- bse/bulk_json_processing.py
+++ bse/bulk_json_processing.py
@@ -2,6 +2,8 @@
 import subprocess
 import sys
 from multiprocessing import Pool as ProcessPool
+#locals
+import mc
 
 def get_data(file):    
     subprocess.call(["D:/Python27/python.exe ./mc.py", file])    
@@ -11,6 +13,8 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print "Usage: bulk_json_processing.py <dir>"
         exit(-1)
-    files = subprocess.check_output(["ls", sys.argv[1] + "*.json"])
-    for file in files:
-        subprocess.call(["D:/Python27/python.exe ./mc.py", file])    
+    folder_path = sys.argv[1].replace('\\','/')
+    files = subprocess.check_output(["ls", folder_path + '/' + "*.json"])
+    for file in files.split('\r\n'):
+        if len(file) > 0:
+            mc.read_json(file)
