17a454dc1e7d02311bdbe3eaed7aafe970f48a2b
YogendraSontakke <yogendra.sontakke@gmail.com>
2015-11-09 16:03:25 +0530

    UpdateData

---
+++ bse/bserun_local.bat
@@ -0,0 +1,2 @@
+D:\Python27\python.exe E:\GitHub\BSENSE\bse\bsedataprocessor.py  E:/GitHub/BSENSE/bse/data/bse_52_week_data/json_daily E:/GitHub/BSENSE/bse/data/bse_52_week_data/json_consolidated
+
--- bse/bserun_testing.bat
+++ bse/bserun_testing.bat
@@ -1,1 +1,1 @@
-D:\Python27\python.exe E:\GitHub\BSENSE\bse\bsedataprocessor.py  E:/Programming/Personal/bse/testing/json_daily E:/Programming/Personal/bse/testing/json_consolidated
+D:\Python27\python.exe E:\GitHub\BSENSE\bse\bsedataprocessor.py  E:/Programming/Personal/bseproject/testing/json_daily E:/Programming/Personal/bseproject/testing/json_consolidated
