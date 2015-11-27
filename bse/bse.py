99eacf1b56784d7eb8a49163a2d1a5b44b6cc62b
YogendraSontakke <yogendra.sontakke@gmail.com>
2015-11-27 19:10:34 +0530

    No Referer Fix

--- bse/bse.py
+++ bse/bse.py
@@ -42,7 +42,8 @@ def get_market_cap_and_name(i_security_code):
     
     if headers['Referer'] == '':
         print 'Referer Missing :', i_security_code
-        return [i_security_code, '0', 'Referer Missing // Not Found']
+        headers['Referer'] = 'http://www.bseindia.com'
+        #return [i_security_code, '0', 'Referer Missing // Not Found']
     
     html = get_post_services.get_html_data(url, params=data, headers=headers)
     while html is None:
